a3e8308dcf5107c4651cd884964adeb8f3d9b506
MIKIEJ-DESKTOP\mikiej <mikiej_boy@hotmail.com>
2017-05-09 22:55:36 +0100

    Added camera lerping from default view to weapon position. Added to camera manager lerping functionality which includes fixed update, start lerping and edit to MoveCameraToAWeaponPos

--- WeaponsManager/Assets/Scripts/Camera/CameraManager.cs
+++ WeaponsManager/Assets/Scripts/Camera/CameraManager.cs
@@ -18,6 +18,27 @@ public class CameraManager : MonoBehaviour {
 	private float fieldOfViewDefault;
 	public float fieldOfViewWeapon;
 
+	#region "Lerping Movement"
+	// The time taken to move from the start to finish positions
+	public float timeTakenDuringLerp = 1f;
+
+	// How far the object should move when 'space' is pressed
+	private float distanceToMove;
+
+	//Whether we are currently interpolating or not
+	private bool _isLerping;
+
+	//The start and finish positions for the interpolation
+	private Vector3 _startPosition;
+	private Vector3 _endPosition;
+
+	//The Time.time value when we started the interpolation
+	private float _timeStartedLerping;
+
+	#endregion
+
+
+
 
 	void Awake()
     {
@@ -116,12 +137,21 @@ public class CameraManager : MonoBehaviour {
      * */
     private void MoveCameraToAWeaponPos(Vector3 pos)
     { 
+		//pos = weapons position to move to
         inDefaultPosition = false;
-		//        var offset = new Vector3(pos.x - 5.5f, pos.y + 1.5f, pos.z - 3);
+
+		_startPosition = cameraDefault.transform.position;
+
 		var CamOffset = new Vector3(pos.x - offsetCamX, pos.y + offsetCamY, pos.z);
 
-		Camera.main.transform.position = CamOffset;
+		_endPosition = CamOffset;
+
+
+		//Camera.main.transform.position = CamOffset;
         Camera.main.transform.LookAt(pos);
+
+		StartLerping();
+
     }
 
 
@@ -224,6 +254,51 @@ public class CameraManager : MonoBehaviour {
 		fieldOfViewWeapon = 75.0f;
 		fieldOfViewDefault = Camera.main.fieldOfView;
 	}
+
+	//We do the actual interpolation in FixedUpdate(), since we're dealing with a rigidbody
+	void FixedUpdate()
+	{
+		if (_isLerping)
+		{
+			//We want percentage = 0.0 when Time.time = _timeStartedLerping
+			//and percentage = 1.0 when Time.time = _timeStartedLerping + timeTakenDuringLerp
+			//In other words, we want to know what percentage of "timeTakenDuringLerp" the value
+			//"Time.time - _timeStartedLerping" is.
+			float timeSinceStarted = Time.time - _timeStartedLerping;
+			float percentageComplete = timeSinceStarted / timeTakenDuringLerp;
+
+			//Perform the actual lerping.  Notice that the first two parameters will always be the same
+			//throughout a single lerp-processs (ie. they won't change until we hit the space-bar again
+			//to start another lerp)
+			Camera.main.transform.position = Vector3.Lerp(_startPosition, _endPosition, percentageComplete);
+			//Constantly update looking at the weapon for smooth rotation
+			Camera.main.transform.LookAt(CurrentWeaponFocus.transform);
+
+			//When we've completed the lerp, we set _isLerping to false
+			if (percentageComplete >= 1.0f)
+			{
+				_isLerping = false;
+				Camera.main.transform.LookAt(CurrentWeaponFocus.transform);
+			}
+		}
+	}
+
+	#region "Lerping Methods"
+
+	void StartLerping()
+	{
+		
+		_timeStartedLerping = Time.time;
+		//Camera.main.transform.LookAt(_endPosition);
+
+		//We set the start position to the current position, and the finish to 10 spaces in the 'forward' direction
+		//_startPosition = transform.position;
+		//distanceToMove = Vector3.Distance(_startPosition, _endPosition);
+		//_endPosition = Camera.main.transform.position + Camera.main.transform.forward * distanceToMove;
+		_isLerping = true;
+	}
+
+	#endregion
 
 
 }
