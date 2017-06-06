d1781898aaa591b499c601ed76b19bf31b356ac4
MIKIEJ-DESKTOP\mikiej <mikiej_boy@hotmail.com>
2017-06-06 22:45:44 +0100

    Added AmmoManager - To handle the ammo that appears in ui for each weapon type Used a MockDb location to recall the projectile types Added Devs Tower model to see how it fits

---
+++ WeaponsManager/Assets/Scripts/Projectiles/AmmoManager.cs
@@ -0,0 +1,41 @@
+﻿using Assets.Scripts.Projectiles;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AmmoManager : MonoBehaviour {
+
+
+	public static AmmoManager instance;
+	private Weapon CurrentWeapon;
+
+	private void Awake()
+	{
+		if(instance == null)
+		{
+			instance = new AmmoManager();
+		}
+		else
+		{
+			Destroy(this);
+		}
+	}
+
+
+	public void SetupAmmoForWeapon(Weapon weapon)
+	{
+		this.CurrentWeapon = weapon;
+
+		//Get Ammo list from DB or equivalent - mock object used for now
+		List<Projectile> projectiles = MockAmmoDatabase.GetAmmoTypes(weapon.name);
+
+		//Create and Populate the UI with List<Projctiles>()
+
+	}
+
+	private void SetUpUIWithAmmoTypesAndCount()
+	{
+
+	}
+
+}
---
+++ WeaponsManager/Assets/Scripts/Projectiles/MockAmmoDatabase.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Text;
+
+namespace Assets.Scripts.Projectiles
+{
+	class MockAmmoDatabase
+	{
+		public static List<Projectile>  GetAmmoTypes(string weaponName)
+		{
+			List<Projectile> projectiles = null;
+			if(!String.IsNullOrEmpty(weaponName))
+			{
+				switch(weaponName)
+				{
+					case "Catapult": projectiles = new List<Projectile>() { new CatapultBall() }; break;
+					case "BALLISTA": projectiles = new List<Projectile>() { new BallistaSpear() }; break;
+				}
+			}
+			else
+			{
+				Debug.WriteLine("Error - pass empty/ null string to AmmoDB");
+			}
+
+			return projectiles;
+
+		}
+
+
+
+	}
+}
